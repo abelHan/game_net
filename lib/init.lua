d5d373e2d141fb04916c8667c66c69eafedb9a96
abel <talance@126.com>
2016-12-07 21:54:34 +0800

    add libs,audio and font

--- lib/init.lua
+++ lib/init.lua
@@ -6,6 +6,9 @@ lib.lovebird = require("lib.lovebird")
 lib.log = lib.lovebird.print
 lib.lovedebug = require("lib.lovedebug")
 
+lib.tween = require("lib.tween")
+lib.audio = require("lib.wave")
+
 function lib.update(dt)
   lib.lovebird.update()
 end
